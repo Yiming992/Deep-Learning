37fd03b2914879eaee951b066cfd89cc4b3c7a06
Yiming Liu <yimingliuno1@gmail.com>
2018-04-26 19:35:11 -0700

    Add files via upload

--- Ultrasound Segmentation/Mask.py
+++ Ultrasound Segmentation/Mask.py
@@ -20,15 +20,7 @@ train_images.sort(key=sort_train_files)
 train_mask=glob.glob('train_mask1/*tif')
 train_mask.sort(key=sort_mask_files)
 
-
-
-
 initializer=tf.variance_scaling_initializer()
-
-def neural_net_input():
-    x=tf.placeholder(tf.float32,[None,None,None,1])
-    return x
-
 def conv_2d(x,output_nums,conv_size,stride,activation='relu'):
     if activation=='relu':
         output=tf.layers.conv2d(x,output_nums,conv_size,strides=stride,padding='same',kernel_initializer=initializer)
@@ -40,17 +32,13 @@ def conv_2d(x,output_nums,conv_size,stride,activation='relu'):
         output=tf.layers.conv2d(x,output_nums,conv_size,strides=stride,padding='same')
     return output
 
-
 def max_pool2d(x,k_size,stride):
     output=tf.layers.max_pooling2d(x,k_size,strides=stride,padding='same')
     return output
 
 def conv_trans(x,output_nums,conv_size,stride):
     output=tf.layers.conv2d_transpose(x,output_nums,conv_size,strides=stride,padding='same')
-
     return output
-
-
 
 tf.reset_default_graph()
 import tensorlayer as tl
@@ -59,8 +47,9 @@ Graph=tf.Graph()
 
 with Graph.as_default():
 
-    Input=neural_net_input()
-    mask=tf.placeholder(tf.int32,[None,None,None,1])
+    Input=tf.placeholder(tf.float32,[None,80,112,1])
+    mask=tf.placeholder(tf.float32,[None,None,None,1])
+    L=tf.placeholder(tf.float32,[None,1])
     with tf.name_scope('conv_3x3_0'):
         conv0=conv_2d(Input,16,[3,3],1)
         conv0_1=conv_2d(conv0,16,[3,3],1)
@@ -79,6 +68,10 @@ with Graph.as_default():
     with tf.name_scope('conv_3x3_3'):
         conv5=conv_2d(pool2,128,[3,3],1)
         conv6=conv_2d(conv5,128,[3,3],1)
+    with tf.name_scope('aux_out'):
+        flat1=tf.layers.flatten(conv6)
+        dense=tf.layers.dense(flat1,1)
+        prob=tf.nn.sigmoid(dense)
     with tf.name_scope('upconv_1'):
         upconv1=conv_trans(conv6,64,[3,3],2)
     with tf.name_scope('concat_1'):
@@ -102,66 +95,69 @@ with Graph.as_default():
         conv12=conv_2d(conv11,16,[3,3],1)
     with tf.name_scope('final_layer'):
         Final=conv_2d(conv12,1,[1,1],1,activation='sigmoid')
-        Final_mask=tf.cast(Final>0.5,dtype=tf.float32)
+        # Final_mask=tf.cast(Final>=0.3,dtype=tf.float32)
 
 
 
     with tf.name_scope('loss'):
         ### Compute 1-(Dice Coeficient) as network's loss function
-        dice_loss=1-tl.cost.dice_coe(Final,tf.cast(mask,tf.float32),axis=[0,1,2,3])
-#         entropy=tf.nn.sparse_softmax_cross_entropy_with_logits(labels=mask,logits=Final)
+        aux_loss=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=L,logits=dense))
+        dice_loss=1-tl.cost.dice_coe(Final,tf.cast(mask,tf.float32),axis=[1,2,3])
+        # Loss=dice_loss
+        Loss=0.5*aux_loss+2*dice_loss
+#        entropy=tf.nn.sparse_softmax_cross_entropy_with_logits(labels=mask,logits=Final)
 #         loss=tf.reduce_mean(entropy)
-        loss_summ=tf.summary.scalar('cost',dice_loss)
+        loss_summ=tf.summary.scalar('cost',Loss)
 
     optimizer=tf.train.AdamOptimizer(learning_rate=0.001,beta1=0.9)
-    train_op=optimizer.minimize(dice_loss)
+    train_op=optimizer.minimize(Loss)
     file_writer=tf.summary.FileWriter(log_file,Graph)
     init_op=tf.group(tf.global_variables_initializer(),tf.local_variables_initializer())
 
-
 if __name__=='__main__':
-
-
-    def get_batch(images,masks,batch_size,seed=0):
+    labels=np.load('labels.pk')
+    def get_batch(images,masks,labels,batch_size,seed=0):
         images_array=np.reshape(np.array(images),(len(images),1))
         masks_array=np.reshape(np.array(masks),(len(masks),1))
         np.random.seed(seed)
         shuffled_images=np.random.permutation(images_array)
         np.random.seed(seed)
         shuffled_masks=np.random.permutation(masks_array)
+        np.random.seed(seed)
+        shuffled_label=np.random.permutation(labels)
         for i in range(len(images)//batch_size):
             img=[]
             lab=[]
             sub_images=shuffled_images[i*batch_size:(i+1)*batch_size]
             sub_masks=shuffled_masks[i*batch_size:(i+1)*batch_size]
+            sub_label=shuffled_label[i*batch_size:(i+1)*batch_size,:]
             for i in range(sub_images.shape[0]):
                 imgs=cv2.imread(sub_images[i][0])
                 labels=cv2.imread(sub_masks[i][0])
-                imgs=imgs[0]
-                labels=labels[0]
-                imgs=cv2.resize(imgs,(80,112),interpolation=cv2.INTER_AREA)
-                labels=cv2.resize(labels,(80,112),interpolation=cv2.INTER_AREA)
+                imgs=imgs[:,:,0]
+                labels=labels[:,:,0]
+                imgs=cv2.resize(imgs,(112,80),interpolation=cv2.INTER_AREA)
+                labels=cv2.resize(labels,(112,80),interpolation=cv2.INTER_AREA)
                 img.append(imgs)
                 lab.append(labels)
-            yield np.stack(img),np.stack(lab)
+            yield np.stack(img),np.stack(lab),sub_label
 
-
-
-
-    save_file='./image_segmentation3/seg'
-    num_epochs=20
+    save_file='./image_segmentation/seg'
+    num_epochs=10
     batch_size=64
     iter=0
     with tf.Session(graph=Graph) as sess:
         sess.run(init_op)
         for i in range(num_epochs):
-            for img,lab in get_batch(train_images,train_mask,batch_size,seed=i):
+            for img,lab,l in get_batch(train_images,train_mask,labels,batch_size,seed=i):
                 iter+=1
                 img=np.expand_dims(img,axis=-1)
                 lab=np.expand_dims(lab,axis=-1)
-                sess.run(train_op,feed_dict={Input:img/255.0,mask:lab/255.0})
+                sess.run(train_op,feed_dict={Input:img/255.0,mask:lab/255.0,L:l})
                 if iter%10==0:
-                    summary_val=loss_summ.eval(feed_dict={Input:img,mask:lab})
+                    summary_val=loss_summ.eval(feed_dict={Input:img/255.0,mask:lab/255.0,L:l})
                     file_writer.add_summary(summary_val,iter)
-            current_loss=sess.run(dice_loss,feed_dict={Input:img,mask:lab})
+            current_loss=sess.run(Loss,feed_dict={Input:img/255.0,mask:lab/255.0,L:l})
             print('Epoch:{}/{}...... Loss: {}'.format(i+1,num_epochs,current_loss))
+        Saver=tf.train.Saver()
+        Saver.save(sess,save_file)
--- Ultrasound Segmentation/Test.py
+++ Ultrasound Segmentation/Test.py
@@ -23,16 +23,18 @@ if __name__=='__main__':
         loader=tf.train.import_meta_graph('./image_segmentation/seg.meta')
         loader.restore(sess,tf.train.latest_checkpoint('./image_segmentation/'))
         X=loaded_graph.get_tensor_by_name('Placeholder:0')
-        mask=loaded_graph.get_tensor_by_name('final_layer/Cast:0')
+        mask=loaded_graph.get_tensor_by_name('final_layer/Sigmoid:0')
         for i in range(len(test_images)):
             test_img=cv2.imread(test_images[i])
             test_img=test_img[:,:,0]
-            resized_img=cv2.resize(test_img,(80,112),interpolation=cv2.INTER_AREA)
+            resized_img=cv2.resize(test_img,(112,80),interpolation=cv2.INTER_AREA)
             resized_img=resized_img[np.newaxis,:,:,np.newaxis]
-            Mask=sess.run(mask,feed_dict={X:resized_img})
+            Mask=sess.run(mask,feed_dict={X:resized_img/255.0})
             Mask=np.squeeze(Mask)
-            Mask=cv2.resize(Mask*255.0,(420,580),interpolation=cv2.INTER_AREA)
+            Mask=cv2.resize(Mask,(580,420),interpolation=cv2.INTER_AREA)
+            Mask=Mask>0.5
+            Mask=Mask*255.0
             # cv2.imshow('{}'.format(i),Mask)
             if not os.path.isdir(os.path.join(current_wd+mask_dir)):
-                os.mkdir(mask_dir)
+                os.mkdir(os.path.join(current_wd+mask_dir))
             cv2.imwrite(os.path.join(current_wd+mask_dir+'\\{}_mask.png'.format(i+1)),Mask)
